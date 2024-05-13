3aabadc47ff6696b197c1100fe536707a346334c
47sec <karevo1@mail.ru>
2024-05-14 00:59:01 +0300

    Update From Home PC

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -6,7 +6,8 @@ public class TestScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello, World from steamdeck!");
+        Debug.Log("Hello, World from steamdeck!");  //Karevo SteamDeck
+        Debug.Log("Hello, World! FromKarevo Home PC");   //KarevoHomePC
     }
 
     // Update is called once per frame

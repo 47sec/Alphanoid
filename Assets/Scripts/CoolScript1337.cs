1fc90cb8b518c81fbd792cea45c0f8e2fbc3c093
Артём <artemrepin00@gmail.com>
2024-05-14 17:33:28 +0300

    Test message
    Starting

---
+++ Assets/Scripts/CoolScript1337.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class CoolScript1337 : MonoBehaviour
+{
+    CoolScript1337()
+    {
+        print();
+    }
+    void print()
+    {
+        Debug.Log("Testo");
+    }
+}

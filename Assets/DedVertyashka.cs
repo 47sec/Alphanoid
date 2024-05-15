0b9060517be846fa3922780d89d9510807098515
liarfmioli <liarfmioli@gmail.com>
2024-05-16 01:46:39 +0300

    дед вертяшка
    дед вертяшка тудудудуду

---
+++ Assets/DedVertyashka.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DedVertyashka : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("DED VERTYASHKA!!! TUDUDUDUDU");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 1);
+    }
+}

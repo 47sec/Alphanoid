dbceb1e008a02a441d146b997349f4c15094b0ba
Артём <artemrepin00@gmail.com>
2024-05-16 14:03:00 +0300

    Misc
    Comments, platform clamp in -10, 10

--- Assets/Scripts/Player/PlatformMovement.cs
+++ Assets/Scripts/Player/PlatformMovement.cs
@@ -14,6 +14,8 @@ public class PlatformMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D)) { transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y); }
         if (Input.GetKey(KeyCode.A)) { transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y); }
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -10f, 10f), transform.position.y); 
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -22,19 +24,24 @@ public class PlatformMovement : MonoBehaviour
         {
             Rigidbody2D ball_rb = other.attachedRigidbody;
 
-            float direction_x = new Vector2(ball_rb.transform.position.x - transform.position.x, 0).normalized.x;
+            float distance = ball_rb.transform.position.x - transform.position.x;
 
-            float center_clamp = Mathf.Floor(Mathf.Abs(ball_rb.transform.position.x - transform.position.x) * 2.5f) + 0.1f;
+            float direction_x = new Vector2(distance, 0).normalized.x; // Сторона, в которую будет отскок
 
-            //Debug.Log(direction_x);
+            float center_size = 2.2f; // Меньше число, больше центр
+
+            float max_angle = 8f; // Больше число, больше угол отскока на краях
+
+            float center_clamp = Mathf.Floor(Mathf.Clamp(Mathf.Abs(distance) * center_size, 0, 1)) * Mathf.Abs(distance) + 0.05f;
+            Debug.Log(center_clamp);
 
             ball_rb.velocity =
                 (
                     ball_rb.velocity *
                     (new Vector2(0f, -1f)) +
-                    (new Vector2(center_clamp * direction_x * 8, 0f))
+                    (new Vector2(center_clamp * direction_x * max_angle, 0f))
                 ).normalized
-                * speed;
+                * (ball_rb.velocity / ball_rb.velocity.normalized);
         }
     }
 }

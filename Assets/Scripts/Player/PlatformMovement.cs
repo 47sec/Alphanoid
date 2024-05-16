1f06059480b094d406fe8a85327618d231cfdcb7
Артём <artemrepin00@gmail.com>
2024-05-16 16:20:04 +0300

    Moved const variables to public
    And added tooltips for them

--- Assets/Scripts/Player/PlatformMovement.cs
+++ Assets/Scripts/Player/PlatformMovement.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
+    [Tooltip("Скорость платформы")]
     public float speed;
 
     void Start()
     {
-        speed = 8;
     }
 
     void FixedUpdate()
@@ -15,8 +15,13 @@ public class PlatformMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) { transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y); }
         if (Input.GetKey(KeyCode.A)) { transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y); }
 
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -10f, 10f), transform.position.y); 
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -10f, 10f), transform.position.y);
     }
+    [Tooltip("Больше число, больше угол отскока на краях")]
+    public float angleMod = 8f;
+
+    [Tooltip("Меньше число, больше зона центра")]
+    public float centerSize = 2.2f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -28,18 +33,14 @@ public class PlatformMovement : MonoBehaviour
 
             float direction_x = new Vector2(distance, 0).normalized.x; // Сторона, в которую будет отскок
 
-            float center_size = 2.2f; // Меньше число, больше центр
-
-            float max_angle = 8f; // Больше число, больше угол отскока на краях
-
-            float center_clamp = Mathf.Floor(Mathf.Clamp(Mathf.Abs(distance) * center_size, 0, 1)) * Mathf.Abs(distance) + 0.05f;
+            float center_clamp = Mathf.Floor(Mathf.Clamp(Mathf.Abs(distance) * centerSize, 0, 1)) * Mathf.Abs(distance) + 0.05f;
             Debug.Log(center_clamp);
 
             ball_rb.velocity =
                 (
                     ball_rb.velocity *
                     (new Vector2(0f, -1f)) +
-                    (new Vector2(center_clamp * direction_x * max_angle, 0f))
+                    (new Vector2(center_clamp * direction_x * angleMod, 0f))
                 ).normalized
                 * (ball_rb.velocity / ball_rb.velocity.normalized);
         }

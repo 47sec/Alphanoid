0cda6297ee99f3e5cd46d8c1a19cccf023bd674b
Артём <artemrepin00@gmail.com>
2024-06-20 05:47:59 +0300

    Additional ball 80%
    Награда для доп. мяча на поле (мяч исчезает, если не поймать, но не наносит урон игроку). Большая проблема с тем, что они отскакивают друг от друга

--- Assets/Scripts/Ball/BallMove.cs
+++ Assets/Scripts/Ball/BallMove.cs
@@ -1,11 +1,12 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class MoveBall : MonoBehaviour
 {
     Rigidbody2D rb;
-    private bool ballIsActivated;
+    Collider2D collider2;
+    private bool ballIsActivated = false;
+    private bool isAdditional = false;
 
     [Tooltip("Платформа")]
     public Transform platform;
@@ -21,7 +22,8 @@ public class MoveBall : MonoBehaviour
 
     private void Start()
     {
-        ballIsActivated = false;
+        rb = GetComponent<Rigidbody2D>();
+        collider2 = GetComponent<Collider2D>();
     }
 
     public bool getActive()
@@ -31,17 +33,20 @@ public class MoveBall : MonoBehaviour
 
     void Update()
     {
-        if (!ballIsActivated && Input.GetKeyDown(KeyCode.Space))
+        if (!ballIsActivated)
         {
-            BallActivate();
+            transform.position = new Vector2(platform.position.x, transform.position.y);
+            collider2.enabled = false;
         }
-        if (!ballIsActivated) { transform.position = new Vector2(platform.position.x, transform.position.y); }
     }
     public void BallActivate()
     {
-        ballIsActivated = true;
+        transform.position = new Vector3(0, platform.GetComponent<BoxCollider2D>().size.y + platform.GetComponent<BoxCollider2D>().size.y * 0.05f) + platform.position;
         rb = GetComponent<Rigidbody2D>();
+        collider2 = GetComponent<Collider2D>();
+        ballIsActivated = true;
         rb.velocity = Quaternion.AngleAxis(Random.Range(-45, 45), Vector3.forward) * (new Vector2(0, 1) * speed);
+        collider2.enabled = true;
     }
 
     public void disable()
@@ -49,13 +54,25 @@ public class MoveBall : MonoBehaviour
         ballIsActivated = false;
     }
 
+    public void setAdditional(bool additional)
+    {
+        this.isAdditional = additional;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.gameObject.tag)
         {
             case "Lose":
+                if (isAdditional)
+                {
+                    Destroy(transform.gameObject);
+                    break;
+                }
+
                 platform.SendMessage("Damaged", 1);
                 ballIsActivated = false;
+                collider2.enabled = false;
                 if (saveMomentum)
                     speed = rb.velocity.magnitude;
                 rb.velocity = Vector2.zero;
